6ec0e564d40a8ed9b3c5fa8e05b235fbaf4aaac0
Gabriel Medeiros <gabrielmedeirosvino@gmail.com>
2022-12-11 15:55:46 -0300

    feat: customers Routes and Controllers

---
+++ src/controllers/customers.controller.js
@@ -0,0 +1,47 @@
+import dayjs from "dayjs";
+import { connectionDB } from "../database/db.js";
+
+export async function create(req, res){
+    const {name, phone, cpf, birthday} = req.body
+
+    try{
+        await connectionDB.query('INSERT INTO customers (name, phone, cpf, birthday) VALUES ($1, $2, $3, $4);',[name, phone, cpf, birthday])
+        res.send(201)
+    }catch(err){
+        res.status(500).send(err)
+    }
+}
+
+export async function findAll(req, res){
+
+    try{
+        const all = await connectionDB.query('SELECT * FROM customers;')
+        res.send(all.rows)
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+}
+
+export async function findById(req, res){
+    const {id} = req.params
+
+    try{
+        const findId = await connectionDB.query(`SELECT * FROM customers WHERE id=$1`, [id])
+        res.send(findId.rows[0])
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+
+}
+
+export async function updateCustomer(req, res){
+    const {id} = req.params
+    const {name, phone, cpf, birthday} = req.body
+
+    try{
+        await connectionDB.query('UPDATE customers SET name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5;', [name, phone, cpf, birthday, id])
+        res.send(200)
+    }catch(err){
+        res.status(500).send(err)
+    }
+}
